6d3ec350d18be3705e5853f731d1f062796039ce
ROWEN-BAK <9018934@student.zadkine.nl>
2024-10-01 14:59:59 +0200

    3 - Fixed

--- 3/app.js
+++ 3/app.js
@@ -1,7 +1,32 @@
-gsap.timeline()
-.from(".text1", {duration: 5, opacity: 0})
-.from(".text2", {duration: 5, x: 500, opacity: 0, ease: "bounce"})
-.from(".text3", {duration: 5, y: 10, opacity: 0, rotation: 720, ease: "back"})
-.from(".text4", {duration: 5, scale: 4, opacity: 0, ease: "elastic"})
-.from(".text5", {duration: 5, y:-50, opacity: 0, rotation: 360, ease: "bounce"})
-.from(".text6",{duration: 5, scale: 7, opacity: 0, ease: "back"})
+function Uno(){
+var tl = gsap.timeline({ defaults: {opacity: 0, duration: 2, ease:"back"}})
+tl.from('.text1', {rotation: 720, y:160, scale: 0.7})
+.from ('.text2', {y:160, scale: 0.7})
+return tl
+}
+
+function Dos(){
+    var tl = gsap.timeline({ defaults: {opacity: 0, duration: 4}})
+    tl.from('.text3', {rotation: 1440, x:500, y:160, scale: 0.1, ease:"expo"})
+    .from('.text4', {y:250, scale:0.3, ease:"elastic"})
+    return tl
+}
+
+function Tres(){
+    var tl = gsap.timeline({ defaults: {opacity: 0, duration: 3}})
+    tl.from('.text5', {y:500, scale:0.4, rotation:2880, ease:"expo"})
+    .from('.text6', {y:300, scale:2, ease:"elastic"})
+    return tl
+}
+
+var master = gsap.timeline();
+master
+.add(Uno(), "+=3")
+.add(Dos(), "+=5")
+.add(Tres(), "-=3")
+
+document.getElementById("play").onclick = ()=> master.play()
+document.getElementById("pause").onclick = ()=> master.pause()
+document.getElementById("reverse").onclick = ()=> master.reverse()
+document.getElementById("restart").onclick = ()=> master.restart()
+
